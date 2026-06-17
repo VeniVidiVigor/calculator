5190f9250f7cdccaa1aa6e9edc809e8030cc5cd0
Kirill Vyazikov <VeniVidiVigorY@yandex.ru>
2026-06-17 22:32:57 +0300

    Реализация части логики '=', функция replaceMathSymbols для корректной передачи операторов и мелкие правки

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,49 @@
-const display = document.querySelector('.calc-container-input')
-const buttons = document.querySelectorAll('button')
-const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-const operators = ['+', '-', '×', '÷', '%', '=']
-const specialButtons = ['⌫', 'AC', '±', ',']
+const display = document.querySelector(".calc-container-input");
+const buttons = document.querySelectorAll("button");
+const operators = ["+", "-", "×", "÷", "%"];
+const specialButtons = ["⌫", "AC", "±", ","];
 
-buttons.forEach(button => {
+buttons.forEach((button) => {
   const value = button.textContent;
-  button.addEventListener('click', () => {
-    console.log(value)
-    if (specialButtons.includes(value)) {
-      console.log('Здесь спец.символ')
-      if (value === 'AC') {
-        display.value = ""
-      } else if (value === '⌫') {
-        display.value = display.value.slice(0, -1)
+  button.addEventListener("click", () => {
+    console.log(value);
+    if (specialButtons.includes(value)) { // обработка спец.кнопок
+      console.log("Здесь спец.символ");
+      if (value === "AC") {
+        display.value = "";
+      } else if (value === "⌫") {
+        display.value = display.value.slice(0, -1);
       }
-    } else if (operators.includes(value)) {
-      console.log('Здесь оператор')
+    } else if (operators.includes(value)) { // обработка математических операторов
+      console.log("Здесь оператор");
       if (display.value === "") {
-        alert('Сначала цифра')
+        alert("Сначала цифра");
       } else if (operators.includes(display.value.at(-1))) {
-        console.log(`Меняю на другого оператора ${value}`)
-        display.value = display.value.slice(0, -1) + value
+        console.log(`Меняю на другого оператора ${value}`);
+        display.value = display.value.slice(0, -1) + value;
       } else {
-        display.value += value
+        display.value += value;
       }
+    } else if (value === "=") { // обработка вычисления
+      if (operators.includes(display.value.at(-1))) {
+        return // просто вернуть без изменений
+      }
+      const expression = replaceMathSymbols(display.value)
+      console.log(expression)
+      display.value = "выполнено действие"; //@todo
     } else {
       display.value += value
     }
-  })
-})
+  });
+});
 
-console.log("Оно дышит")
+function calculate() {
+  // вычисление
+}
+
+function replaceMathSymbols(value) {
+  const replaceSymbols = value
+    .replaceAll('×', '*')
+    .replaceAll('÷', '/');
+  return replaceSymbols
+}

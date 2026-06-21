d66ff5d0f8cd15d58362eb4bace553679afdf0af
Kirill Vyazikov <VeniVidiVigorY@yandex.ru>
2026-06-21 15:23:58 +0300

    Реализация вычисления, добавление проверок числа, мелкие доработки

--- src/js/script.js
+++ src/js/script.js
@@ -7,15 +7,28 @@ buttons.forEach((button) => {
   const value = button.textContent;
   button.addEventListener("click", () => {
     console.log(value);
-    if (specialButtons.includes(value)) { // обработка спец.кнопок
-      console.log("Здесь спец.символ");
+    if (specialButtons.includes(value)) {
+      // обработка спец.кнопок
       if (value === "AC") {
         display.value = "";
       } else if (value === "⌫") {
         display.value = display.value.slice(0, -1);
+      } else if (value === ",") {
+        if (
+          display.value
+            .split(/[+\-×÷%]/)
+            .at(-1)
+            .includes(",") ||
+          operators.includes(display.value.at(-1)) ||
+          display.value === ""
+        ) {
+          return;
+        } else {
+          display.value += ",";
+        }
       }
-    } else if (operators.includes(value)) { // обработка математических операторов
-      console.log("Здесь оператор");
+    } else if (operators.includes(value)) {
+      // обработка математических операторов
       if (display.value === "") {
         alert("Сначала цифра");
       } else if (operators.includes(display.value.at(-1))) {
@@ -24,26 +37,41 @@ buttons.forEach((button) => {
       } else {
         display.value += value;
       }
-    } else if (value === "=") { // обработка вычисления
-      if (operators.includes(display.value.at(-1))) {
-        return // просто вернуть без изменений
+    } else if (value === "=") {
+      // обработка вычисления
+      if (operators.includes(display.value.at(-1)) || display.value === "") {
+        return;
       }
-      const expression = replaceMathSymbols(display.value)
-      console.log(expression)
-      display.value = "выполнено действие"; //@todo
+      const expression = replaceMathSymbols(display.value);
+      const result = calculate(expression);
+      if (String(result).includes(".")) {
+        display.value = result.toFixed(2);
+      } else {
+        display.value = result;
+      }
     } else {
-      display.value += value
+      display.value += value;
     }
   });
 });
 
-function calculate() {
-  // вычисление
+function calculate(expression) {
+  try {
+    const result = eval(expression);
+    if (!Number.isFinite(result)) {
+      return "Нельзя делить на ноль";
+    } else {
+      return result;
+    }
+  } catch (error) {
+    return "Error";
+  }
 }
 
 function replaceMathSymbols(value) {
   const replaceSymbols = value
-    .replaceAll('×', '*')
-    .replaceAll('÷', '/');
-  return replaceSymbols
+    .replaceAll("×", "*")
+    .replaceAll("÷", "/")
+    .replaceAll(",", ".");
+  return replaceSymbols;
 }
